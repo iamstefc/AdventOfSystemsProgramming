61610a28ab7fb6667e36cd3f26e19f17b421abde
jefitzge <jefitzge@syr.edu>
2017-03-04 14:57:08 -0500

    Part 3 helpful information

---
+++ HW_5_Part3.h
@@ -0,0 +1,9 @@
+void printhelp()
+{
+	printf("Usage: %s [ARG][OPTION] ", argv[0]);
+	printf("[ARG] [PATH TO FILE BEING COPIED]");
+	printf("-d, --customize the backup location,    d takes the path to the backup folder\n");
+	printf("-m, --disables meta-data duplication,    m takes the name of the file you are copying\n");
+	printf("-t, --append the duplication time to the file name,    t takes no arguments\n");
+}
+	
