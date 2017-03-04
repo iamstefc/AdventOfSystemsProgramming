3669fa79d7d28042836355dcebafdd7eb97921c6
stefanochiarella <spchiare@syr.edu>
2017-03-04 15:26:10 -0500

    I deleted the copy function as it produced many errors

--- myCopyFIle.h
+++ myCopyFIle.h
@@ -1,38 +1,5 @@
 #include<stdio.h>
 
-	int copy_file(char *old_filename, char  *new_filename)
-	{
-		FILE  *ptr_old, *ptr_new;
-		errno_t err = 0, err1 = 0;
-		int  a;
-
-		err = fopen_s(&ptr_old, old_filename, "rb");
-		err1 = fopen_s(&ptr_new, new_filename, "wb");
-
-		if(err != 0)
-			return  -1;
-
-		if(err1 != 0)
-		{
-			fclose(ptr_old);
-			return  -1;
-		}
-
-		while(1)
-		{
-			a  =  fgetc(ptr_old);
-
-			if(!feof(ptr_old))
-				fputc(a, ptr_new);
-			else
-				break;
-		}
-
-		fclose(ptr_new);
-		fclose(ptr_old);
-		return  0;
-	}
-
 	int  main(void)
 	{
 		
