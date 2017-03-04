a830f3a1ed24711497143d8a8e45495649a4ef46
jefitzge <jefitzge@syr.edu>
2017-03-04 15:00:14 -0500

    changed arguments and added part 3 helpful information

--- copy.c
+++ copy.c
@@ -2,18 +2,19 @@
 #include "stdlib.h"
 #include "stdbool.h"
 #include "unistd.h"
+#include "HW_5_Part3.h"
 
 int main(int argc, char* argv[])
 {
 	bool opt_a =false, opt_b = false, opt_d =false;
-	int opt = getopt(argc, argv, "da:b");
+	int opt = getopt(argc, argv, "md:t");
 
 	if(argv[1] = ""){
 		printf("Usage: %s\n",argv[0]);
 
 	}
 
-  /* while (opt != -1)
+  /while (opt != -1)
    {
       if (opt == 'p') {
          opt_p = true;
@@ -22,9 +23,9 @@ int main(int argc, char* argv[])
       } else if (opt == 'u') {
          opt_u = true;
       }
-      opt = getopt(argc, argv, "da:b");
+      opt = getopt(argc, argv, "md:t");
    }
-*/
+
 
 
 
