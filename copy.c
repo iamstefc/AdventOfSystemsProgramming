b8c654c603ceacc7e9171cec8ae9d09dbf581592
mcrispin13 <mescoto13@gmail.com>
2017-03-04 14:01:48 -0500

    pt 2 print usage information

--- copy.c
+++ copy.c
@@ -1,13 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdbool.h"
+#include "unistd.h"
 
-int main(){
+int main(int argc, char* argv[])
+{
+	bool opt_a =false, opt_b = false, opt_d =false;
+	int opt = getopt(argc, argv, "da:b");
 
-printf("Here's my comment(Milton)");
-printf("(Hello World! This is my comment (Stefano)");
+	if(argv[1] = ""){
+		printf("Usage: %s\n",argv[0]);
+
+	}
+
+  /* while (opt != -1)
+   {
+      if (opt == 'p') {
+         opt_p = true;
+      } else if (opt == 'e') {
+         opt_e = true;
+      } else if (opt == 'u') {
+         opt_u = true;
+      }
+      opt = getopt(argc, argv, "da:b");
+   }
+*/
 
 
 
 
-return EXIT_SUCCESS; 
+
 }
