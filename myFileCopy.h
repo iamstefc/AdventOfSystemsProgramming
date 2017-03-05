75961a77a380c4ddee8ff98be9595939b04b3c0c
jefitzge <jefitzge@syr.edu>
2017-03-04 19:08:41 -0500

    added incomplete watch method

--- myFileCopy.h
+++ myFileCopy.h
@@ -2,42 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-int copy(char* name)
+
+void copy(char* name)
 {
 	//source: http://www.programmingsimplified.com/c-program-copy-file
    char ch, target_file[20]="";
 	char *gets(char *str);
    
    FILE *source, *target;
- 
-   
- 
+
    source = fopen(name, "r");
- 
    if( source == NULL )
    {
-      printf("Press any key to exit...\n");
       exit(EXIT_FAILURE);
    }
  
- 
-  \
    target = fopen("temp", "w");
- 
+
    if( target == NULL )
    {
       fclose(source);
-      
       exit(EXIT_FAILURE);
    }
- 
+
    while( ( ch = fgetc(source) ) != EOF )
-      fputc(ch, target);
- 
-   printf("File copied successfully.\n");
- 
+   {
+	   fputc(ch, target);
+   }
+printf("File copied successfully.\n");
+
    fclose(source);
    fclose(target);
- 
-   return 0;
 }
