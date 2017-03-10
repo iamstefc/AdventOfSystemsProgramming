5b83f0a29b82a7221b8cca7e6200d794992ae96e
stefanochiarella <spchiare@syr.edu>
2017-03-10 18:49:35 -0500

    I am adding the backup.h file and updating the copy.c to fit the new included file

--- copy.c
+++ copy.c
@@ -5,9 +5,19 @@
 #include "printhelp.h"
 #include "myFileCopy.h"
 #include "watch.h"
+#include "backup.h"
 
 int main(int argc, char* argv[])
-{  char* name = argv[1];
+{  
+	char* name = argv[1];
+    if (argc != 2){
+        return 0;
+    } 
+    if (stat(source, &pro) < 0){ 
+        printf("Error sourcing");
+        return 0;
+    }
+	
 	bool opt_m =false, opt_d = false, opt_t =false;
 	int opt = getopt(argc, argv, "md:t");
 
@@ -16,6 +26,7 @@ int main(int argc, char* argv[])
 
 	}
    copy(name);
+	backup();
   watch(name);
  
 
