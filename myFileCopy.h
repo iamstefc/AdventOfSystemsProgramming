36f081ed141cb508cf1f492297ebc58da1babcdc
stefanochiarella <spchiare@syr.edu>
2017-03-11 18:41:56 -0500

    File takes in user input to copy files/needs fixing

--- myFileCopy.h
+++ myFileCopy.h
@@ -6,31 +6,39 @@
 void copy(char* name)
 {
 	//source: http://www.programmingsimplified.com/c-program-copy-file
-   char ch, target_file[20]="";
-	char *gets(char *str);
-   
+   char ch, source_file[20], target_file[20];
    FILE *source, *target;
-
-   source = fopen(name, "r");
+ 
+   printf("Enter name of file to copy\n");
+   gets(source_file);
+ 
+   source = fopen(source_file, "r");
+ 
    if( source == NULL )
    {
+      printf("Press any key to exit...\n");
       exit(EXIT_FAILURE);
    }
-	
-   target = fopen("temp", "w");
-
+ 
+   printf("Enter name of target file\n");
+   gets(target_file);
+ 
+   target = fopen(target_file, "w");
+ 
    if( target == NULL )
    {
       fclose(source);
+      printf("Press any key to exit...\n");
       exit(EXIT_FAILURE);
    }
-
+ 
    while( ( ch = fgetc(source) ) != EOF )
-   {
-	   fputc(ch, target);
-   }
-printf("File copied successfully.\n");
-
+      fputc(ch, target);
+ 
+   printf("File copied successfully.\n");
+ 
    fclose(source);
    fclose(target);
+ 
+   return 0;
 }
