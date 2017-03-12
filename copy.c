2e9d646a1c0810387c1019a331291ba2eced3e74
mcrispin13 <mescoto13@gmail.com>
2017-03-12 04:47:07 -0400

    part 7 2 1

--- copy.c
+++ copy.c
@@ -3,43 +3,225 @@
 #include "stdbool.h"
 #include "unistd.h"
 #include "printhelp.h"
-#include "myFileCopy.h"
 #include "watch.h"
-#include "backup.h"
+#include "myFileCopy.h"
+#include "sys/stat.h"
+#include "errno.h"
+#include "string.h"
+#include "pwd.h"
+#include "grp.h"
+
 
 int main(int argc, char* argv[])
 {  
-	char* name = argv[1];
-    if (argc != 2){
-        return 0;
-    } 
-    if (stat(source, &pro) < 0){ 
-        printf("Error sourcing");
-        return 0;
+	char* source = "~/Backup";
+	const char* name = argv[1];
+	char* arg_d;
+	int opt = getopt(argc, argv, "mhd:t");
+	bool opt_m =false, opt_d = false, opt_t =false, opt_h =false;
+	ssize_t EVENT_SIZE = (sizeof (struct inotify_event));
+	ssize_t EVENT_BUF_LEN = (1024 * (EVENT_SIZE + 16));
+	
+	
+
+	if(argv[1] == NULL){
+		printf("Usage: %sEXECUTABLE\n",argv[0]);	//part 2: if no arguments provided
+		return 1;
+	}
+    
+   
+   struct stat file_stat;
+   if (stat(name, &file_stat) < 0){ 
+        printf("Error sourcing\n");					//if source file does not exist
+        return 1;
     }
 	
-	bool opt_m =false, opt_d = false, opt_t =false;
-	int opt = getopt(argc, argv, "md:t");
-
-	if(argv[1] == ""){
-		printf("Usage: %sEXECUTABLE\n",argv[0]);
-
-	}
-   copy(name);
-	backup();
-  watch(name);
- 
-
-
-  while (opt != -1)
+	
+													//checks command line arguments
+	while (opt != -1)
    {
       if (opt == 'm') {
          opt_m = true;
       } else if (opt == 'd') {
-         opt_d= true;
+         opt_d = true;
+         arg_d = optarg;
       } else if (opt == 't') {
          opt_t = true;
+      } else if (opt == 'h') {
+      	 opt_h = true;
       }
-      opt = getopt(argc, argv, "md:t");
+      opt = getopt(argc, argv, "mhd:t");
    }
+
+   if(opt_h){printhelp(name); 							//prints out helpful info
+   		return 0;}
+   		
+
+
+
+	//backup();
+  //watch(name);
+
+char* home = getenv("HOME");
+char str[80]="";
+strcpy(str,home);
+strcat(str,"/backup");
+char* path = str;
+struct stat s;
+int err = stat(source,&s);
+
+if (opt_d){
+	err = stat(path,&s);
+	}
+
+if(-1 == err) {
+if(ENOENT == errno){ 
+	if(opt_d){ 
+	path = arg_d;}											//directory does not exist
+	mkdir(path, S_IRWXG | S_IRWXO | S_IRWXU);
+	printf("Backup Locations: %s\n",path);
+       
+    } 
+}
+else{
+	printf("Directory Already Exists\n");
+}
+
+char n_copy[100];
+char* current = "_rev0";
+int count = 0;
+
+current = n_copy;
+
+
+strcpy(str,path);
+strcat(str,"/temp");
+path = str;
+
+char* temp = path;
+
+copy(name,temp);
+
+
+
+
+
+
+
+
+
+//INOTIFY ------------------------
+
+/*int length, i = 0;
+  int fd;
+  int wd;
+  char buffer[EVENT_BUF_LEN];
+
+ 
+  fd = inotify_init();
+
+  
+  if ( fd < 0 ) {
+    perror( "inotify_init" );
+  }
+wd = inotify_add_watch( fd, name, IN_MODIFY| IN_DELETE );
+
+  /*read to determine the event change happens on “/tmp” directory. Actually this read blocks until the change event occurs*/ 
+
+  /*length = read( fd, buffer, EVENT_BUF_LEN ); 
+
+  /**if ( length < 0 ) {
+    perror( "read" );
+  }  
+
+ /* /*actually read return the list of change events happens. Here, read the change event one by one and process it accordingly.*/
+ /* while ( i < length ) {     
+  	
+  	struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];     
+  	if ( event->len ) {
+     
+        if ( event->mask & IN_MODIFY ) {
+          printf( " %s MODIFIED.\n", event->name );
+        }
+      
+      else if ( event->mask & IN_DELETE ) {
+        if ( event->mask & IN_ISDIR ) {
+          printf( "Directory %s deleted.\n", event->name );
+        }
+        
+      }
+    }
+    i += EVENT_SIZE + event->len;
+  }	*/
+
+	
+	
+					
+					/*strcpy(n_copy,"");
+					strcat(n_copy, name);
+					strcat(n_copy,"_rev");
+
+					printf("%d\n",count );
+					char m[999];
+					sprintf(m, "%d", count);
+					temp = m;
+					strcat(n_copy,temp);
+					current = n_copy;
+					copy(name,current);
+					count++;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*stat(name, &file_stat);
+struct group* gr = getgrgid(file_stat.st_gid);
+struct passwd* pw = getpwuid(file_stat.st_uid);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+
+
+
 }
--- printhelp.h
+++ printhelp.h
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-void printhelp(char* name)
+void printhelp(const char* name)
 {
 	printf("Usage: %s [ARG][OPTION] \n", name);
-	printf("[ARG] [PATH TO FILE BEING COPIED]");
+	printf("[ARG] [PATH TO FILE BEING COPIED]\n");
 	printf("-d, --customize the backup location,    d takes the path to the backup folder\n");
 	printf("-m, --disables meta-data duplication,    m takes the name of the file you are copying\n");
 	printf("-t, --append the duplication time to the file name,    t takes no arguments\n");
